35b27ff5c8c581b5b09e871f7759861746902ca4
Gustavo Garcia <goostavobg@gmail.com>
2020-10-23 18:52:19 -0300

    Added message and user parsing capabilities

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
+const fs = require('fs');
+const whatsapp = require('whatsapp-chat-parser');
+const parseUserList = require('./parseUserList');
 
+console.log('Loading files!')
+const rawHistory = fs.readFileSync('zapzap.txt', 'utf8');
+const rawUsers = fs.readFileSync('userList.txt', 'utf8');
 
-let firstMessage;
-let lastMessage;
+async function runSync(){
+  console.log('Parsing files')
+  let history;
+  let users;
+  try {
+    history = await whatsapp.parseString(rawHistory);
+    users = parseUserList(rawUsers);
+  } catch (err) {
+    console.log(err);
+  }
+  console.log('Loaded:', history.length, 'messages.')
+  console.log('Loaded:', users.length, 'users.')
 
+}
 
-var lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream('zapzap.txt')
-});
-
-lineReader.on('line', function (line) {
-  console.log('Line from file:', line.length);
-});
-
-let isMessage = function(row){
-
-  return false;
-};
-
+runSync();
