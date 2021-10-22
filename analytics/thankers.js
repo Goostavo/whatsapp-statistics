ec6ce8697f68fd2de4690b48033713f4144587fb
juan luiz <juanlews@hotmail.com>
2021-10-22 20:47:52 -0300

    gratidão 🙏

--- analytics/thankers.js
+++ analytics/thankers.js
@@ -6,20 +6,20 @@
 module.exports = function(history) {
     let thanksMessages = 0;
     let kindPeople = {};
-    
+
     for (let entry of history){
-      if (entry.message.match(/Obrigado|Obrigada|obrigado|obrigada|Valeu|valeu|vlw|obg|thx|thanks|thank|brigado|brigada|obrigado!|obrigada!|Obrigado!|Obrigada!|obg!|thx!|thanks!|thank!|brigado!|brigada!/)) {
+      if (entry.message.match(/Obrigado|Obrigada|obrigado|obrigada|Valeu|valeu|vlw|obg|thx|thanks|thank|brigado|brigada|obrigado!|obrigada!|Obrigado!|Obrigada!|obg!|thx!|thanks!|thank!|brigado!|brigada!|🙏/)) {
         thanksMessages += 1;
-        
+
         if (!kindPeople[entry.author]){
           kindPeople[entry.author] = 0;
         }
         kindPeople[entry.author] += 1;
       }
     }
-    
-    console.log('Total de thankers:', thanksMessages)
-    
+
+    console.log('\nTotal de Mr./Ms. Gratidão 🙏:', thanksMessages)
+
     let countArray = [];
     for (let key in kindPeople){
       countArray.push({
@@ -29,14 +29,14 @@ module.exports = function(history) {
     }
     countArray = countArray.sort(compare).reverse();
     // Print top 10 users in friendly format
-    console.log('Top 10 thankers:')
+    console.log('Top 10 Mr./Ms. Gratidão 🙏:')
     for (let userCount = 0; userCount < 10; userCount++){
       console.log('@' + countArray[userCount].author, ':', countArray[userCount].count)
     }
     console.log();
-    
+
   }
-  
+
   function compare( a, b ) {
     if ( a.count < b.count ){
       return -1;
